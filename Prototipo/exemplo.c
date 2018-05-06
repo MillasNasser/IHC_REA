cac2e2c0061e04da78f88abefc8ac805748b8e5b
Paulo Tobias <paulohtobias@outlook.com>
2018-05-06 20:28:25 -0300

    Leitor de código por Instruções

--- Prototipo/exemplo.c
+++ Prototipo/exemplo.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 
 void preencheVetor(int *vet, int size){
-    int i;
-    vet = (int*) malloc(sizeof(int) * size);
-    for(i=0; i < size; i++){
-        vet[i] = i;
-    }
-    for(i=0; i < size; i++){
-        printf("%d ",vet[i]);
-    }
-    printf("\n");
+	int i;
+	vet = (int*) malloc(sizeof(int) * size);
+	for(i=0; i < size; i++){
+		vet[i] = i;
+	}
+	for(i=0; i < size; i++){
+		printf("%d ",vet[i]);
+	}
+	printf("\n");
 }
 
 int main(){
-    char c = 'c';
-    int i;
-    int size = 5;
-    int *vetor;
-    
-    preencheVetor(vetor, size);
-    
-    printf("Antes do for\n");
-    for(i=0; i < size; i++){
-        printf("%d ",vetor[i]);
-    }
-    printf("Depois do for\n");
+	char c = 'c';
+	int i;
+	int size = 5;
+	int *vetor;
 
-    return 0;
+	preencheVetor(vetor, size);
+
+	printf("Antes do for\n");
+	for(i=0; i < size; i++){
+		printf("%d ",vetor[i]);
+	}
+	printf("Depois do for\n");
+
+	return 0;
 }
