cd80c60505dc60136410c9967e897a91ace5b89d
Millas N <millas5444@gmail.com>
2018-06-23 19:56:50 -0300

    Mudança para barra lateral

--- Prototipo/avaliacao.php
+++ Prototipo/avaliacao.php
@@ -19,112 +19,129 @@
 </head>
 
 <body onload="Start()">
-	<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-		<ul class="navbar-nav">
-			<li class="nav-item">
-				<a class="nav-link" href="index.php">
-					<span class="glyphicon glyphicon-home"></span>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="NovoExercicio.html">Registrar exercício</a>
-			</li>
-		</ul>
-	</nav>
-
 	<div class="container-fluid">
-		<div class="btn-group">
-			<button class="btn btn-info btn-sm" disabled onclick="voltar()">
-				<span class="glyphicon glyphicon-chevron-left"> Voltar</span>
-			</button>
-			<button class="btn btn-info btn-sm" onclick="avançar();">
-				<span class="glyphicon glyphicon-chevron-right"> Próximo</span>
-			</button>
-		</div>
-		<div class="btn-group">
-			<button class="btn btn-danger btn-sm" onclick="restartTable();">
-				<span class="glyphicon glyphicon-repeat"> Resetar Exercício</span>
-			</button>
-		</div>
-	</div><br/>
-
-	<?php
-		$exercicio = NULL;
-		if(!empty($_POST)){
-			$arquivo = "./exercicios/".$_POST['arquivo'];
-
-			$executavel = substr( $arquivo,0 , strpos($arquivo,".c"));
-			system("gcc -g ".$arquivo." -o ".$executavel);
-			$sistema = popen("python2 parser.py ".$executavel,"r");
-			$exercicio = fopen($arquivo,'r');
-			$JSON = stream_get_contents($sistema);
-			
-			echo
-			"<script>
-				entradaRequisitos = JSON.parse('".str_replace("'","\'",$JSON)."');
-			</script>";
-		}else{
-			echo "Não carregou o post";
-		}
-	?>
-
-	<div class="container-fluid row">
-		<div class="col-sm-6 d-inline-flex">
-			<div id="LineNumbers" class="Numbers Dark-Base"></div>
-			<div id="Code" class="Code w-100">
-				<?php
-					if($exercicio != NULL){
-						/*echo 
-						'<script>
-							document.getElementById("Code").innerHTML = "";
-						</script>';*/
-						$i = 0;
-						for($i = 1; !feof($exercicio); $i++){
-							
-							if(fscanf($exercicio,"%[^\n]\n",$leitura)==0){
-								$leitura = '';
-							}
-							
-							$instrucao = " ".str_replace('<', '&lt;', $leitura);
-							$instrucao = str_replace("\\n", "\\\\n", $instrucao);
-							$instrucao = str_replace('"', '\\"', $instrucao);
-							//echo $instrucao;
-							echo 
-							'<script>
-								var pre = document.createElement("pre");
-								pre.className = "prettyprint prettyprinted";
-								pre.id = "linha_" + '.$i.';
-						
-								pre.innerHTML = PR.prettyPrintOne("'.$instrucao.'", "C", true);
-								document.getElementById("Code").appendChild(pre);
+		<div class="row h-100">
+			<aside class="col-12 col-md-2 p-0 bg-dark">
+				<nav class="navbar navbar-expand navbar-dark bg-dark flex-row">
+					<div class="collapse navbar-collapse">
+						<div class="flex-md-column flex-row navbar-nav w-100 justify-content-between">
+							<ul class="list-unstyled components w-100">
+								<li class="nav-item">
+									<a class="nav-link text-center" href="index.php">
+										<span class="glyphicon glyphicon-home"></span> Home
+									</a>
+								</li>
 								
-							</script>';
-						}
-						$i--;
-						echo 
-						'<script>
-							genLinesNum('.$i.');
-						</script>';
+								<li class="nav-item">
+									<a class="nav-link text-center" href="NovoExercicio.html">
+										<span class="glyphicon glyphicon-floppy-save"></span>Criar exercício
+									</a>
+								</li>
+								
+								<li><hr/></li>
+								
+								<li>
+									<button class="btn btn-info btn-sm w-100 m-1" disabled onclick="voltar()">
+										<span class="glyphicon glyphicon-chevron-left pull-left"></span>Voltar
+									</button>
+								</li>
+								<li>
+									<button class="btn btn-info btn-sm w-100 m-1" onclick="avançar();">
+										<span class="glyphicon glyphicon-chevron-right pull-left"></span>Verificar
+									</button>
+								</li>
+								<li>
+									<button class="btn btn-danger btn-sm w-100 m-1" onclick="restartTable();">
+										<span class="glyphicon glyphicon-repeat"></span>Resetar
+									</button>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</aside>
+			<main class="row col-12 col-md-10" style="overflow: auto;">
+				<?php
+					$exercicio = NULL;
+					if(!empty($_POST)){
+						$arquivo = "./exercicios/".$_POST['arquivo'];
+			
+						$executavel = substr( $arquivo,0 , strpos($arquivo,".c"));
+						system("gcc -g ".$arquivo." -o ".$executavel);
+						$sistema = popen("python2 parser.py ".$executavel,"r");
+						$exercicio = fopen($arquivo,'r');
+						$JSON = stream_get_contents($sistema);
+						
+						echo
+						"<script>
+							entradaRequisitos = JSON.parse('".str_replace("'","\'",$JSON)."');
+						</script>";
+					}else{
+						echo "Não carregou o post";
 					}
 				?>
-			</div>
-		</div>
 
-		<table class="table table-striped col-sm-6">
-			<thead>
-				<tr class="MemHeader">
-					<th class="table-dark MCol1">Endereço</th>
-					<th class="MCol2">Nome da Variavel</th>
-					<th class="MCol3">Valor</th>
-					<th class="MCol4">Valor Desreferênciado</th>
-				</tr>
-			</thead>
-			<tbody id="MemTabl">
-			</tbody>
-		</table>
+				<div class="container m-0 ml-1 mt-1 p-0 col-12 col-xl-6">
+					<div id="LineNumbers" class="Numbers Dark-Base col-1"></div>
+
+					<div id="Code" class="Code col-11">
+						<?php
+							if($exercicio != NULL){
+								/*echo 
+								'<script>
+									document.getElementById("Code").innerHTML = "";
+								</script>';*/
+								$i = 0;
+								for($i = 1; !feof($exercicio); $i++){
+									
+									if(fscanf($exercicio,"%[^\n]\n",$leitura)==0){
+										$leitura = '';
+									}
+									
+									$instrucao = " ".str_replace('<', '&lt;', $leitura);
+									$instrucao = str_replace("\\n", "\\\\n", $instrucao);
+									$instrucao = str_replace('"', '\\"', $instrucao);
+									//echo $instrucao;
+									echo 
+									'<script>
+										var pre = document.createElement("pre");
+										pre.className = "prettyprint prettyprinted";
+										pre.id = "linha_" + '.$i.';
+								
+										pre.innerHTML = PR.prettyPrintOne("'.$instrucao.'", "C", true);
+										document.getElementById("Code").appendChild(pre);
+										
+									</script>';
+								}
+								$i--;
+								echo 
+								'<script>
+									genLinesNum('.$i.');
+								</script>';
+							}
+						?>
+					</div>
+				</div>
+
+				<div class="col-12 m-0 ml-1 mt-1 p-0 col-xl-5">
+					<table class="table table-striped"> 
+						<thead> 
+							<tr class="MemHeader"> 
+								<th class="table-dark MCol1">Endereço</th> 
+								<th class="MCol2">Nome da Variavel</th> 
+								<th class="MCol3">Valor</th> 
+								<th class="MCol4">Valor Desreferênciado</th> 
+							</tr> 
+						</thead> 
+						<tbody id="MemTabl"> 
+						</tbody> 
+					</table> 
+				</div>
+			</main>
+		</div>
 	</div>
-	<br/>
-	<br/>
+
+	
 </body>
 
 </html>
--- Prototipo/index.php
+++ Prototipo/index.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html>
 
 <head>
@@ -18,7 +17,70 @@
 
 <body>
     <!-- Header da página -->
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <div class="container-fluid">
+        <div class="row h-100">
+                <aside class="col-12 col-md-2 p-0 bg-dark">
+                    <nav class="navbar navbar-expand navbar-dark bg-dark flex-row">
+                        <div class="collapse navbar-collapse">
+                            <div class="flex-md-column flex-row navbar-nav w-100 justify-content-between">
+                                <ul class="list-unstyled components w-100">
+                                    <li class="nav-item">
+                                        <a class="nav-link text-center" href="index.php">
+                                            <span class="glyphicon glyphicon-home"></span> Home
+                                        </a>
+                                    </li>
+
+                                    
+                                    <li class="nav-item">
+                                        <a class="nav-link text-center" href="NovoExercicio.html">
+                                            <span class="glyphicon glyphicon-floppy-save"></span>Criar exercício
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </aside>
+                <main class="row col-12 col-md-9 m-2" style="overflow: auto;">
+                    <form class="w-100" action="avaliacao.php" method="post" id="loadExercicios">
+                        <h1 class="display-4"> Carregar exercício </h1>
+                        <div class="container-fluid">
+                            <table class="table table-striped table-hover row-clickable">
+                                <thead>
+                                    <tr>
+                                        <th>Nome</th>
+                                        <th>Dificuldade</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="ListaDeExercicios">
+                                    <?php
+                                        $exDir = scandir("./exercicios/");
+                                        if($exDir == false){
+                                            echo "Olha a mentira";
+                                        }
+                                        foreach($exDir as &$dir){
+                                            if(substr($dir, -2) === ".c"){
+                                                $estrelas = 1;
+                                                echo "<tr>
+                                                        <td><a href='#' onclick='retornoClick(\"".$dir."\");'>".$dir."</a></td>
+                                                        <td>";
+                                                        for($i = 0 ; $i < $estrelas; $i++){
+                                                            echo "<span class='glyphicon glyphicon-star'></span>";
+                                                        }
+                                                echo "</td></tr>";
+                                            }
+                                        }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <input type="hidden" name="arquivo" value="" id="NomeArquivo"/>
+                    </form>
+                </main>
+            </div>
+        </div>
+
+    <!-- <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
         <ul class="navbar-nav">
             <li class="nav-item">
                 <a class="nav-link active" href="index.php">
@@ -29,42 +91,10 @@
                 <a class="nav-link" href="NovoExercicio.html">Registrar exercício</a>
             </li>
         </ul>
-    </nav>
+    </nav> -->
 
     <!-- Tabela dos links -->
-    <form action="avaliacao.php" method="post" id="loadExercicios">
-        <div class="container">
-            <table class="table table-striped table-hover row-clickable">
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Dificuldade</th>
-                    </tr>
-                </thead>
-                <tbody id="ListaDeExercicios">
-                    <?php
-                        $exDir = scandir("/mnt/981C548C1C546772/Dropbox/UFSJ/0_TP's/2018-1/IHC/REA/Prototipo/exercicios/");
-                        if($exDir == false){
-                            echo "Olha a mentira";
-                        }
-                        foreach($exDir as &$dir){
-                            if(substr($dir, -2) === ".c"){
-                                $estrelas = 1;
-                                echo "<tr>
-                                        <td><a href='#' onclick='retornoClick(\"".$dir."\");'>".$dir."</a></td>
-                                        <td>";
-                                        for($i = 0 ; $i < $estrelas; $i++){
-                                            echo "<span class='glyphicon glyphicon-star'></span>";
-                                        }
-                                echo "</td></tr>";
-                            }
-                        }
-                    ?>
-                </tbody>
-            </table>
-        </div>
-        <input type="hidden" name="arquivo" value="" id="NomeArquivo"/>
-    </form>
+
 </body>
 
 </html>
